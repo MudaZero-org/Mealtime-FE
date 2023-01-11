0ed9ced2eca0fc1abf95cf25e3af2f1ac4e1b794
Ken <kenbuslonkinoshita@gmail.com>
2023-01-11 19:43:04 +0900

    fix: adding files
    Co-authored-by: iAmKenKinoshita <iamkenkinoshita@users.noreply.github.com>
    Co-authored-by: brian-walvoord <brian-walvoord@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import User from "./pages/userAuth/User";
+import SignIn from "./pages/userAuth/SignIn";
+import SignUp from "./pages/userAuth/SignUp";
+import Homepage from "./pages/userAuth/Homepage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [activeMealPacks, setActiveMealPacks] = useState(null);
+  const [pastMealPacks, setPastMealPacks] = useState(null);
+
+	return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<SignIn />} />
+        <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/user" element={<User />} />
+        <Route exact path="/home" element={
+          <Homepage 
+            activeMealPacks={activeMealPacks} 
+            setActiveMealPacks={setActiveMealPacks} 
+            pastMealPacks={pastMealPacks} 
+            setPastMealPacks={setPastMealPacks} 
+          />
+        } />
+      </Routes>
+    </BrowserRouter>
+	);
 }
 
 export default App;
