d78b8c6a389d8d02fe8cd4fde908669a0ce78b21
Brian Walvoord <brian.walvoord@gmail.com>
2023-01-11 21:39:45 +0900

    feat: added input field and button for ingredients to homepage

--- src/pages/userAuth/Homepage.jsx
+++ src/pages/userAuth/Homepage.jsx
@@ -12,12 +12,17 @@ const Homepage = (props) => {
 	return (
 		<div>
 			<h1>This is the homepage</h1>
+			<div className="input-container">
+				<p className="input-instructions">Type or paste ingredients below<br></br><em>(each ingredient on a new line)</em></p>
+				<textarea className="input-box" cols="50" rows="10" placeholder="eggplant&#10;white rice&#10;daikon"></textarea>
+				<button className="generate-button">Generate Meal Packs</button>
+			</div>
 			<ActiveView
 				activeMealPacks={activeMealPacks}
 				setActiveMealPacks={setActiveMealPacks}
 			/>
       <button>Edit Active Meal Packs</button>
-			<PastView pastMealPacks={pastMealPacks} setPastMealPacks={setPastMealPacks} />       {/* Add overlay to add back to active meal pack list */}
+			<PastView pastMealPacks={pastMealPacks} setPastMealPacks={setPastMealPacks} />
 		</div>
 	);
 };
