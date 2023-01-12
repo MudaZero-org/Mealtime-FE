1176895f326d5bd4d866adae86e38b99d68882df
Brian Walvoord <brian.walvoord@gmail.com>
2023-01-12 09:29:32 +0900

    feat: ingredientArr state created from user input as array

--- src/pages/userAuth/Homepage.jsx
+++ src/pages/userAuth/Homepage.jsx
@@ -1,5 +1,6 @@
 import ActiveView from "./components/ActiveView";
 import PastView from "./components/PastView";
+import { useState, useEffect, useRef } from "react";
 
 const Homepage = (props) => {
 	const {
@@ -9,13 +10,30 @@ const Homepage = (props) => {
 		setPastMealPacks,
 	} = props;
 
+	const [text, setText] = useState(null)
+	const [ingredientArr, setIngredientArr] = useState([])
+	const isMounted = useRef(false)
+
+	const makeIngredientArr = () => {
+		setIngredientArr(text.split(/\r?\n/))
+	}
+
+	// prints ingredient array to console
+	useEffect(() => {
+		if (isMounted.current) {
+			console.log(ingredientArr)
+		} else {
+			isMounted.current = true;
+		}
+	}, [ingredientArr])
+
 	return (
 		<div>
 			<h1>This is the homepage</h1>
 			<div className="input-container">
-				<p className="input-instructions">Type or paste ingredients below<br></br><em>(each ingredient on a new line)</em></p>
-				<textarea className="input-box" cols="50" rows="10" placeholder="eggplant&#10;white rice&#10;daikon"></textarea>
-				<button className="generate-button">Generate Meal Packs</button>
+				<p className="input-instructions">Type or copy/paste ingredients below<br></br><em>(each ingredient on a new line)</em></p>
+				<textarea onChange={(e) => setText(e.target.value)} className="input-box" cols="50" rows="10" placeholder="eggplant&#10;white rice&#10;daikon&#10;chicken thigh"></textarea>
+				<button onClick={makeIngredientArr} className="generate-button">Generate Meal Packs</button>
 			</div>
 			<ActiveView
 				activeMealPacks={activeMealPacks}
