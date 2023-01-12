61c20baeb64e51992ee74a588126e1222d0b6218
Brian Walvoord <brian.walvoord@gmail.com>
2023-01-12 21:41:03 +0900

    feat: added meal-pack page and clickable link to navigate

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import User from "./pages/userAuth/User";
 import SignIn from "./pages/userAuth/SignIn";
 import SignUp from "./pages/userAuth/SignUp";
 import Homepage from "./pages/userAuth/Homepage";
+import MealPack from "./pages/userAuth/MealPack";
 
 function App() {
   const [activeMealPacks, setActiveMealPacks] = useState(null);
   const [pastMealPacks, setPastMealPacks] = useState(null);
+  const [selectedActivePack, setSelectedActivePack] = useState(null);
 
   return (
     <BrowserRouter>
@@ -22,12 +24,21 @@ function App() {
           path="/home"
           element={
             <Homepage
+              selectedActivePack={selectedActivePack}
+              setSelectedActivePack={setSelectedActivePack}
               activeMealPacks={activeMealPacks}
               setActiveMealPacks={setActiveMealPacks}
               pastMealPacks={pastMealPacks}
               setPastMealPacks={setPastMealPacks}
             />
           }
+        />
+        <Route
+          selectedActivePack={selectedActivePack}
+          setSelectedActivePack={setSelectedActivePack}
+          exact
+          path="/meal-pack"
+          element={<MealPack />}
         />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/userAuth/MealPack.jsx
@@ -0,0 +1,10 @@
+const MealPack = (props) => {
+  const { selectedActivePack, setSelectedActivePack} = props;
+
+  return (
+    <div>
+      <h1>This is the meal pack page</h1>
+    </div>
+  )
+}
+export default MealPack;
