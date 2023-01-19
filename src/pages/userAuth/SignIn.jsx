a7fef2ff5db6397078a45721d830609606dd12c4
Brian Walvoord <brian.walvoord@gmail.com>
2023-01-19 10:29:11 +0900

    style: added bulma, styled signin page

--- src/pages/userAuth/SignIn.jsx
+++ src/pages/userAuth/SignIn.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../../styles/pages/_signin.scss";
 
 import AuthUtils from "./utils/authenticate";
 
@@ -20,65 +21,73 @@ const SignIn = (props) => {
 	};
 
 	return (
-		<div>
-			This is from the sign in
-			<form>
-				<label>Email Address:</label>
-				<input
-					type="text"
-					placeholder="Email"
-					onChange={(e) => {
-						setEmail(e.target.value);
-					}}
-				></input>
-				<label>Password:</label>
-				<input
-					type="password"
-					placeholder="Password"
-					onChange={(e) => {
-						setPassword(e.target.value);
-					}}
-				></input>
-				<button
-					onClick={async (e) => {
-						e.preventDefault();
-						try {
-							await AuthUtils.logIn(email, password).then((res) => {
-								toTheHomepage();
-							});
-						} catch (error) {
-							// 	//Error Handling
-							// 	if (err === "") {
-							// 		//If there is no error send user to "homepage"
-							// 		// setCurrentView("home")
-							// 	}
-							// });
-						}
-					}}
-				>
-					Submit
-				</button>
-				<div>
-					Don't have an account?
-					<button
-						onClick={() => {
-							reroute();
-						}}
-					>
-						Click here!
-					</button>
+		<div className="signin-app">
+			<div className="card">
+				<div className="card-header has-background-primary-dark">
+					<div className="card-header-title is-centered">
+						<h1 className="card-title">MudaZero</h1>
+					</div>
 				</div>
-				<div>
-					Delete Auth!
-					<button
-						onClick={() => {
-							AuthUtils.logOut();
-						}}
-					>
-						Click here!
-					</button>
+				<div className="card-content">
+					<form>
+						<div className="input-field">
+							<label>Email Address:</label>
+							<input
+								type="text"
+								placeholder="Email"
+								onChange={(e) => {
+									setEmail(e.target.value);
+								}}
+							></input>
+						</div>
+						<div className="input-field">
+							<label>Password:</label>
+							<input
+								type="password"
+								placeholder="Password"
+								onChange={(e) => {
+									setPassword(e.target.value);
+								}}
+							></input>
+						</div>
+						<div className="input-field">
+							<button className="button is-medium"
+								onClick={async (e) => {
+									e.preventDefault();
+									try {
+										await AuthUtils.logIn(email, password).then((res) => {
+											toTheHomepage();
+										});
+									} catch (error) {
+										// 	//Error Handling
+										// 	if (err === "") {
+										// 		//If there is no error send user to "homepage"
+										// 		// setCurrentView("home")
+										// 	}
+										// });
+									}
+								}}
+							>
+								Login
+							</button>
+						</div>
+						<div className="spacer"></div>
+						<div className="input-field">
+							<p>Don't have an account? <a className="register-link" onClick={reroute}>Sign up here!</a></p>
+						</div>
+						<div className="input-field">
+							Delete Auth!
+							<button className="button"
+								onClick={() => {
+									AuthUtils.logOut();
+								}}
+							>
+								Click here!
+							</button>
+						</div>
+					</form>
 				</div>
-			</form>
+			</div>
 		</div>
 	);
 };
