a12c14774300d044919f6a665227db7fd3359631
Brian Walvoord <brian.walvoord@gmail.com>
2023-01-27 14:59:03 +0900

    fix: removed html tags from instructions on pdf

--- src/pages/userAuth/components/ActiveView.jsx
+++ src/pages/userAuth/components/ActiveView.jsx
@@ -55,7 +55,7 @@ const ActiveView = (props) => {
 		const doc = new jsPDF("p", "mm", "a4");
 		let width = doc.internal.pageSize.getWidth();
 		const name = meal.mealpackName;
-		const details = meal.recipeDetail;
+		const instructions = meal.recipeDetail.analyzedInstructions[0].steps.map((e) => " " + e.number + "." + " " + e.step)
 		const ingredients = meal.recipeDetail.extendedIngredients.map((e) => " " + e.name)
 		doc.setFontSize(24);
 		doc.text(10, 90, `${name}`);
@@ -63,7 +63,7 @@ const ActiveView = (props) => {
 		doc.text(10, 110, [`Ingredients: ${ingredients}`], {
 			maxWidth: width / 1.15,
 		})
-		doc.text(10, 140, [`${details.instructions}`], {
+		doc.text(10, 140, [`${instructions}`], {
 			maxWidth: width / 1.15,
 		});
 		doc.addImage(qrCode, "PNG", 10, 15, 50, 50);
