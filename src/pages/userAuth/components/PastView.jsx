fd129c76310853e054d82f366fb2c2faa596357d
Brian Walvoord <brian.walvoord@gmail.com>
2023-02-02 21:18:15 +0900

    feat: add multiple pages to pdf for multiple selected meal packs

--- src/pages/userAuth/components/PastView.jsx
+++ src/pages/userAuth/components/PastView.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { jsPDF } from "jspdf";
 import "../../../styles/pages/_homepage.scss";
 import { v4 as uuidv4 } from "uuid";
 import API_URL, {REACT_APP_URL} from "../../../Constants";
@@ -147,8 +148,52 @@ const PastView = (props) => {
 		}
 	}
 
+	const downloadPDF = (meal) => {
+		let qrCode = generateQRCode(meal);
+		const doc = new jsPDF("p", "mm", "a4");
+		let width = doc.internal.pageSize.getWidth();
+		const name = meal.mealpackName;
+		const instructions = meal.recipeDetail.analyzedInstructions[0].steps.map((e) => " " + e.number + "." + " " + e.step)
+		const ingredients = meal.recipeDetail.extendedIngredients.map((e) => " " + e.name)
+		doc.setFontSize(24);
+		doc.text(10, 90, `${name}`);
+		doc.setFontSize(18);
+		doc.text(10, 110, [`Ingredients: ${ingredients}`], {
+			maxWidth: width / 1.15,
+		})
+		doc.text(10, 140, [`${instructions}`], {
+			maxWidth: width / 1.15,
+		});
+		doc.addImage(qrCode, "PNG", 10, 15, 50, 50);
+		doc.addImage(meal.recipeDetail.image, "JPG", 100, 15, 80, 50)
+		doc.autoPrint({ variant: "non-conform" });
+		doc.save("print-mealpacks.pdf");
+	};
+
 	const downloadAllPDF = () => {
-		console.log("not yet dummy");
+		const doc = new jsPDF("p", "mm", "a4");
+		let width = doc.internal.pageSize.getWidth();
+		selectionArr.map((meal) => {
+			let qrCode = generateQRCode(meal);
+			let name = meal.mealpackName;
+			let instructions = meal.recipeDetail.analyzedInstructions[0].steps.map((e) => " " + e.number + "." + " " + e.step)
+			let ingredients = meal.recipeDetail.extendedIngredients.map((e) => " " + e.name)
+			doc.setFontSize(24);
+			doc.text(10, 90, `${name}`);
+			doc.setFontSize(18);
+			doc.text(10, 110, [`Ingredients: ${ingredients}`], {
+				maxWidth: width / 1.15,
+			})
+			doc.text(10, 140, [`${instructions}`], {
+				maxWidth: width / 1.15,
+			});
+			doc.addImage(qrCode, "PNG", 10, 15, 50, 50);
+			doc.addImage(meal.recipeDetail.image, "JPG", 100, 15, 80, 50)
+			doc.addPage();
+		})
+		let pageCount = doc.internal.getNumberOfPages();
+		doc.deletePage(pageCount);
+		doc.save("print-mealpacks.pdf")
 	};
 
 	const generateQRCode = (meal) => {
@@ -181,6 +226,7 @@ const PastView = (props) => {
                   {e.recipeDetail.dairyFree && <span className="tag" id="dairy">dairy free</span>}
                 </div>
                 <div className="past-mealpacks-buttons">
+									<button key={uuidv4()} className="button" onClick={() => downloadPDF(e)} style={{ marginBottom: "10px" }}>Download PDF</button>
                   <button key={uuidv4()} className="button" onClick={() => {
                     //Old Code
                     // rerouteToMealpack(e)
